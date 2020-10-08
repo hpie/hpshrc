745ca101631fed8c405c6be6fadb853dc8c78a6d
vasimlook <vasimlook@gmail.com>
2020-10-08 19:07:26 +0530

    title sub categories
    title sub categories

--- App/Views/adminside/categories/edit_subcategory.php
+++ App/Views/adminside/categories/edit_subcategory.php
@@ -6,7 +6,7 @@
             <div class="col-md-12 col-sm-12 col-xs-12">
                 <div class="x_panel">
                     <div class="x_title">
-                        <h2>Edit Category</h2>                    
+                        <h2>Edit Sub Category</h2>                    
                         <div class="clearfix"></div>
                     </div>
                     <div class="x_content">
